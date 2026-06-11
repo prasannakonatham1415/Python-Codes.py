a32888b175532865a093e379b98dff69c6ce8935
prasannakonatham1415 <prasannakonatham1514@gmail.com>
2026-06-11 13:24:26 +0530

    Create finding the square and cube of the given number and find the sum of squ and cube of given number.py

---
+++ finding the square and cube of the given number and find the sum of squ and cube of given number.py
@@ -0,0 +1,19 @@
+a=int(input())
+b=a*a
+print(b)  # square
+
+x = 5
+y = 3
+cube=x**y
+print(f"The Cube of Three Numbers {cube}")  # cube
+
+a=6
+b=a*a
+sum1=b
+print(sum1)
+n1=4
+n2=3
+sum2=(n1**n2)
+print(sum2)
+sum=sum1+sum2
+print(sum)  # sum of squ and cube
