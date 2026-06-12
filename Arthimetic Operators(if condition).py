20d298ee782762ef9fcd883157e466d4eb444658
prasannakonatham1415 <prasannakonatham1514@gmail.com>
2026-06-12 22:44:05 +0530

    Create Arthimetic Operators using (if condition).py

---
+++ Arthimetic Operators(if condition).py
@@ -0,0 +1,45 @@
+# Program to add two numbers
+n1=87
+n2=99
+if (n1+n2):
+   print(n1+n2)
+else:
+   print("False")
+
+# program to display quotient and remainder after division
+a = 7
+b = 9
+if a > 0 and b > 0:
+    print("Quotient =", a // b)
+    print("Remainder =", a % b)
+
+# program to find the total and average of 3 numbers
+n1 = 5
+n2 = 7
+n3 = 8
+total = n1 + n2 + n3
+average = total / 3
+if average > 0:
+    print("Total =", total)
+    print("Average =", average)
+
+# program to find the square of given number
+n = 5
+if n > 0:
+    print("Square =", n*n)
+
+# program to find the cube of given number
+a = 6
+if a > 0:
+   print("Cube=",a**3)	
+
+# program to find the sum of square and cube of given number
+a = 5
+if a > 0:
+    print((a ** 2) + (a ** 3))
+
+# program to display the last digit of the given number
+a = 5
+if a > 0:
+    print((a ** 2) + (a ** 3))
+
