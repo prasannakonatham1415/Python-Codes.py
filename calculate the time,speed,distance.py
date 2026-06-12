8168883b35044ea239757380486f4ea699f5f559
prasannakonatham1415 <prasannakonatham1514@gmail.com>
2026-06-12 10:09:38 +0530

    Create calculate the time,speed,distance.py

---
+++ calculate the time,speed,distance.py
@@ -0,0 +1,14 @@
+# calculate the time taken  using speed and distance
+speed=67.5
+distance=5.76
+print(f"The Time Taken={distance/speed}")
+
+# caluculate the speed by using distance and time
+distance=45.9
+time=34.8
+print("The Speed=",(distance/time))
+
+# calculate the distance by using speed and time
+speed=56.8
+time=25.9
+print("The Distance=",(speed*time))
