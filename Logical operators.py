81c38c6ef2fc529623d57798ed4eb18828f9be16
prasannakonatham1415 <prasannakonatham1514@gmail.com>
2026-06-12 11:34:55 +0530

    Create Logical operators.py

---
+++ Logical operators.py
@@ -0,0 +1,108 @@
+# condition for sum of 2 numbers equal to 10
+n1=5
+n2=5
+print(n1+n2==10)    
+
+# condition for sum of 2 numbers multiplication equals to third number
+x=5
+y=6
+z=30
+print(x*y==z)
+
+# condition for avg of 4 numbers is greater than 60
+a=130
+b=130
+c=110
+d=20
+print((a+b+c+d)/4>60)   
+
+#condition for the number is divisble by 7
+n=24
+print(n%7==0)
+
+#condition for the number is not divisble by 100
+n=71
+print(n!=100)
+
+#condition for the number is even
+n=33
+print(n%2==0)
+
+# condition for the Quantity of fruits exactly in dozens
+quantity=48
+if quantity%12==0:
+    print("quantity is exactly in dozens")
+else:
+    print("quantity is not exactly in dozens")
+
+# square of first number equals to cube of second number
+a=8
+b=4
+print(a*2==b*3)
+
+# last digit of num equal to 5
+num=15
+print(num%10==5)
+
+# sum of last digits of given three numbers is equal to 1
+num1=11
+num2=4
+num3=6
+print(num1%10+num2%10+num3%10==1)
+
+# condition for person can vote
+a=29
+if a>=18:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+# last digit of given number divisible by 3   
+x=123
+print((x%10)%3==0)
+
+#average of 3 numbers equal to first number
+a=6
+b=9
+c=3
+print((a+b+c)/3==a)
+
+#square of last digits greater than 10
+a=36
+last_digit=a%10
+print((a**2)>10)
+
+#square of last digit of given num divisible by 3
+x=47
+last_digit=x%10
+print((last_digit**2)%3==0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
