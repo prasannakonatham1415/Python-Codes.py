33eb271c0b790345d99aa093e16855dda4b51738
prasannakonatham1415 <prasannakonatham1514@gmail.com>
2026-06-23 20:54:40 +0530

    Create Binary Numbers Patterns.py

---
+++ Binary Numbers Patterns.py
@@ -0,0 +1,23 @@
+for i in range(1,6):
+    for j in range(1,i+1):
+        if (i+j) % 2 == 0:
+            print("1",end="")
+        else:
+            print("0",end="")
+    print()
+
+for i in range(1,6):
+    for j in range(i):
+        if i % 2 == 1:
+            print("1",end="")
+        else:
+            print("0",end="")
+    print()
+
+for i in range(5, 0, -1):
+    for j in range(i):
+        if i % 2 == 1:
+            print("1", end="")
+        else:
+            print("0", end="")
+    print()
