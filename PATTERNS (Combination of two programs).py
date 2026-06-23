06d53c4f0a9a4eda598498cc0f81ae71d23bb085
prasannakonatham1415 <prasannakonatham1514@gmail.com>
2026-06-23 20:31:06 +0530

    Create PATTERNS (Combination of two programs).py

---
+++ PATTERNS (Combination of two programs).py
@@ -0,0 +1,55 @@
+# Upper Triangle
+for i in range(1,6):
+    for j in range(i):
+        print("*",end="")
+    print()
+# Lower Triangle
+for i in range(4,1,-1):
+    for j in range(i):
+        print("*",end="")
+    print()
+  
+
+# Upper Triangle
+for i in range(1,6):
+    for j in range(i):
+        print("*",end="")
+    print()
+# Lower Triangle
+for i in range(4,1,-1):
+    for j in range(i):
+        print("*",end="")
+    print()
+  
+
+# Upper Triangle
+for i in range(5,0,-1):
+    for j in range(i):
+        print("*",end="")
+    print()
+# Lower Triangle
+for i in range(1,5):
+    for j in range(i+1):
+        print("*",end="")
+    print()
+  
+
+# Upper Triangle
+for i in range(5,0,-1):
+    for s in range(5-i):
+        print(" ",end="")
+    for j in range(i):
+        print("*",end="")
+    print()
+# Lower Triangle
+for i in range(2,6):
+    for s in range(5-i):
+        print(" ",end="")
+    for j in range(i):
+        print("*",end="")
+    print()
+
+
+
+
+
