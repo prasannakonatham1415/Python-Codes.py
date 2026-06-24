0d32bac9245b5871914460a4304ab4d312581b48
prasannakonatham1415 <prasannakonatham1514@gmail.com>
2026-06-24 13:04:34 +0530

    Program to print patterns.py

--- Program to print patterns.py
+++ Program to print patterns.py
@@ -200,3 +200,27 @@ for i in range(1, 6):
         if num == 10:
             num = 1
     print()
+
+for i in range(1,6):
+    for j in range(1, i + 1):
+        if j % 2 == 0:
+            print("0", end="")
+        else:
+            print("1", end="")
+    print()
+
+for i in range(1,6):
+    for j in range(1, i + 1):
+        if i % 2 == 0:
+            print("0", end="")
+        else:
+            print("1", end="")
+    print()
+
+for i in range(1,6):
+    for j in range(6-i):
+        if i% 2 == 0:
+            print("0", end="")
+        else:
+            print("1", end="")
+    print()
