eee7b1aae75c1bbbefe4c68df77d7b98aeb8f231
prasannakonatham1415 <prasannakonatham1514@gmail.com>
2026-07-02 23:00:03 +0530

    Create dictionary .py

---
+++ dictionary .py
@@ -0,0 +1,15 @@
+nums={"a":100,"b":200,"c":300}
+x={101:[1,2,3,4],102:[7,8,9]}
+print(nums)
+print(x[101])
+print(x[102])
+
+emps={"id":101,"name":"Rana","salary":7899.0,"address":"Vijayawada"}
+print(emps)
+
+d=eval(input("Enter Your Value:"))
+print(d)
+
+emps={"id":101,"name":"Rana","salary":6789.00,"address":"Vijayawada"}
+print(f"My Id={emps['id'] } and My Name={emps['name']}")
+print(f"Salary={emps['salary']} and My  Address={emps['address']}")
